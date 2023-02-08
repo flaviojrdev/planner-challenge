ad8d1c1779c002decea5f1679bcb0c8686b0e9c2
Flavio Junior <d-garm@outlook.com>
2023-02-08 14:17:13 -0400

    refactor: move users endpoints to controller folder and cleanup

--- app.js
+++ app.js
@@ -2,25 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// JSON data files
+// JSON data file
 const usersFile = './data/users.json';
 
-// Middlewares
+// Middleware
 app.use(express.json());
 
-/* 
-
-POST ROUTES
-
-*/
-app.post('/users', (req, res) => {
-  // Create New User
-});
-
-app.post('/users/signin', (req, res) => {
-  // Sign In User
-});
-
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+exports.createUser = (req, res) => {
+  // Create New User
+}; 
+
+exports.signInUser = (req, res) => {
+  // Sign In User
+};
+
