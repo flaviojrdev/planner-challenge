591021e8be555e1964c6edc4d6122abdb599aaaf
Flavio Junior <d-garm@outlook.com>
2023-02-08 16:55:57 -0400

    refactor: remove json reference

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// JSON data file
-const usersFile = './data/users.json';
-
 // Middleware
 app.use(express.json());
 
