ecb819ae1c74acabe6137bb225a562881814fa0a
Flavio Junior <d-garm@outlook.com>
2023-02-08 23:37:01 -0400

    feat: add routes for user signUp and signIn

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+// user routes
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+router
+  .route('/users/signUp')
+  .post(userController.signUp);
+
+router
+  .route('/users/signIn')
+  .post(userController.signIn);
+
+module.exports = router;
