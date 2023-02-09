8ec8b91230a43eeffc5bf4e9a7abb7edd0eb0451
Flavio Junior <d-garm@outlook.com>
2023-02-09 03:39:03 -0400

    feat: include id and day routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -8,4 +8,11 @@ router
   .get(eventController.getAllEvents)
   .post(eventController.createEvent);
 
+router
+  .route('/events/:id')
+  .get(eventController.getEvent)
+  .delete(eventController.deleteEvent);
+
+router.route('/events/:day').delete(eventController.deleteEventsByDay);
+
 module.exports = router;
