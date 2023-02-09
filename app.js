edfa26568a19b50bf05ee855ffb251c093355f72
Flavio Junior <d-garm@outlook.com>
2023-02-09 02:13:54 -0400

    feat: fix eventRouter filepath

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const eventRouter = require('./routes/tourRoutes');
+const eventRouter = require('./routes/eventRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // Middleware
