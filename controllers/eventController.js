66c041add355a522fd69be6159156bfca6093868
Flavio Junior <d-garm@outlook.com>
2023-02-09 03:25:37 -0400

    refactor: cleanup code and add error handling

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -12,7 +12,7 @@ exports.checkBody = (req, res, next) => {
   if (!description || !dateTime || !createdAt) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Description, date and time, and created at are required'
+      message: 'Description, date and time, and created at are required',
     });
   }
   next();
@@ -20,70 +20,100 @@ exports.checkBody = (req, res, next) => {
 
 // 3) ROUTE HANDLERS
 exports.getAllEvents = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: events.length,
-    data: {
-      events
+  try {
+    const allEvents = events;
+    if (!allEvents || allEvents.length === 0) {
+      throw new Error('No events found');
     }
-  });
+    res.status(200).json({
+      status: 'success',
+      results: allEvents.length,
+      data: {
+        allEvents,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
 };
 
 exports.getEvent = (req, res) => {
-  const id = req.params.id;
-  const event = events.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      event
+  try {
+    const id = req.params.id;
+    const event = events.find((el) => el.id === id);
+    if (!event) {
+      return res.status(404).json({
+        status: 'fail',
+        message: `Event not found with id: ${id}`,
+      });
     }
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        event,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'An error occurred while retrieving event.',
+    });
+  }
 };
 
 exports.getEventsByDay = (req, res) => {
   const day = req.params.day;
-  const eventsByDay = events.filter(event => event.day === day);
+  const eventsByDay = events.filter((event) => event.day === day);
+  if (!eventsByDay) {
+    return res.status(404).json({
+      status: 'fail',
+      message: `No events found for day: ${day}`,
+    });
+  }
   res.status(200).json({
     status: 'success',
     results: eventsByDay.length,
     data: {
-      eventsByDay
-    }
+      eventsByDay,
+    },
   });
 };
 
 exports.createEvent = (req, res) => {
-  const newEvent = {
-    id: uuid(),
-    ...req.body
-  };
+  const newEvent = { id: uuid(), ...req.body };
   events.push(newEvent);
   fs.writeFile(
     `${__dirname}/../data/events.json`,
     JSON.stringify(events),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          event: newEvent
-        }
-      });
+    (err) => {
+      if (err) return res.status(500).json({ status: 'error', message: err });
+      return res
+        .status(201)
+        .json({ status: 'success', data: { event: newEvent } });
     }
   );
 };
 
 exports.deleteEvent = (req, res) => {
   const id = req.params.id;
-  const event = events.find(el => el.id === id);
-  const index = events.indexOf(event);
-  events.splice(index, 1);
+  const eventIndex = events.findIndex((el) => el.id === id);
+  events.splice(eventIndex, 1);
   fs.writeFile(
     `${__dirname}/../data/events.json`,
     JSON.stringify(events),
-    err => {
-      res.status(204).json({
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Error deleting event',
+        });
+      }
+      return res.status(204).json({
         status: 'success',
-        data: null
+        data: null,
       });
     }
   );
@@ -91,16 +121,20 @@ exports.deleteEvent = (req, res) => {
 
 exports.deleteEventsByDay = (req, res) => {
   const day = req.params.day;
-  const eventsByDay = events.filter(event => event.day === day);
-  const index = events.indexOf(eventsByDay);
-  events.splice(index, 1);
+  events = events.filter((event) => event.day !== day);
   fs.writeFile(
     `${__dirname}/../data/events.json`,
     JSON.stringify(events),
-    err => {
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Could not delete events by day',
+        });
+      }
       res.status(204).json({
         status: 'success',
-        data: null
+        data: null,
       });
     }
   );
