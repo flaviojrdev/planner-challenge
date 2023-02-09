2ee1e396e7520e9ff7283032b04d0933bd9438a6
Flavio Junior <d-garm@outlook.com>
2023-02-09 02:12:31 -0400

    feat: import and use routes

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const eventRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 // Middleware
 app.use(express.json());
+
+// Routes
+app.use('/api/v1/events', eventRouter);
+app.use('/api/v1/users', userRouter);
 
 // Start server
 app.listen(port, () => {
