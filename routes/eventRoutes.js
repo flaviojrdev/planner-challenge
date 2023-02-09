9dd8d6473d78aef11cd2b3bfc6819284fd087b88
Flavio Junior <d-garm@outlook.com>
2023-02-08 23:29:53 -0400

    feat: add baseline route for /events

---
+++ routes/eventRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const eventController = require('../controllers/eventController');
+
+const router = express.Router();
+
+router
+  .route('/events')
+  .get(eventController.getAllEvents)
+  .post(eventController.createEvent);
+
+module.exports = router;
